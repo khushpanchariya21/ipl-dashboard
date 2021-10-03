88b6ec34dd3840f34572ad2309d7a62becba82e6
Khush Panchariya <khushx@Khushs-MacBook-Pro.local>
2021-10-03 12:30:11 +0530

    updated dist

---
+++ precache-manifest.9efa0cf591f7e9663d8aca8fbc5c0c5a.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187b47628e0f40c54af6f495be75505c",
+    "url": "/ipl-dashboard/404.html"
+  },
+  {
+    "revision": "71a295b2484fedf8eeca",
+    "url": "/ipl-dashboard/css/app.913d0137.css"
+  },
+  {
+    "revision": "c0480434631ffce68964",
+    "url": "/ipl-dashboard/css/chunk-vendors.ac0834e4.css"
+  },
+  {
+    "revision": "25cd1d254a61fc384b70141abbdad407",
+    "url": "/ipl-dashboard/img/Chennai-Super-Kings.25cd1d25.png"
+  },
+  {
+    "revision": "6a9e3f34223bfe719a851a9e4d6cfb7d",
+    "url": "/ipl-dashboard/img/Deccan-Chargers.6a9e3f34.png"
+  },
+  {
+    "revision": "291c918a524c26cf803c806af3308347",
+    "url": "/ipl-dashboard/img/Delhi-Daredevils.291c918a.png"
+  },
+  {
+    "revision": "32aadc4127f96736589f6fa4a155ad5f",
+    "url": "/ipl-dashboard/img/Gujarat-Lions.32aadc41.png"
+  },
+  {
+    "revision": "fb4fcd96ab1c26c0d8122561e78da1ef",
+    "url": "/ipl-dashboard/img/Kings-XI-Punjab.fb4fcd96.png"
+  },
+  {
+    "revision": "8b133ad9275ac2039db9e3a09678aff5",
+    "url": "/ipl-dashboard/img/Kochi-Tuskers-Kerala.8b133ad9.png"
+  },
+  {
+    "revision": "5506602b187fe16a617fa126a116f5c3",
+    "url": "/ipl-dashboard/img/Kolkata-Knight-Riders.5506602b.png"
+  },
+  {
+    "revision": "fe91dd9886742af13b8b9edf54dd92d5",
+    "url": "/ipl-dashboard/img/Mumbai-Indians.fe91dd98.png"
+  },
+  {
+    "revision": "688ca3cdbb0603d981047a0611362b5d",
+    "url": "/ipl-dashboard/img/Pune-Warriors.688ca3cd.png"
+  },
+  {
+    "revision": "a9acd75702580bb562006871fafe2347",
+    "url": "/ipl-dashboard/img/Rajasthan-Royals.a9acd757.png"
+  },
+  {
+    "revision": "ad48584916d264f511daf3ac00ca70b2",
+    "url": "/ipl-dashboard/img/Rising-Pune-Supergiant.ad485849.png"
+  },
+  {
+    "revision": "0857d22c6cd01d053d2fe5547586142b",
+    "url": "/ipl-dashboard/img/Royal-Challengers-Bangalore.0857d22c.png"
+  },
+  {
+    "revision": "ccad15fb71070c250b6678a58feaf224",
+    "url": "/ipl-dashboard/img/Sunrisers-Hyderabad.ccad15fb.png"
+  },
+  {
+    "revision": "18db18091f87531797186cdc6669192e",
+    "url": "/ipl-dashboard/img/calendar.18db1809.svg"
+  },
+  {
+    "revision": "11da897bb828d30f1c5160492aa71ef3",
+    "url": "/ipl-dashboard/img/clear.11da897b.svg"
+  },
+  {
+    "revision": "d4af38314e5c44d8115bdcd56645ef44",
+    "url": "/ipl-dashboard/img/fans.d4af3831.png"
+  },
+  {
+    "revision": "4b5a33c46d5d5090831b8b573e6abbdd",
+    "url": "/ipl-dashboard/img/github-logo.4b5a33c4.svg"
+  },
+  {
+    "revision": "291c34d25063fb301675c35da8427b4f",
+    "url": "/ipl-dashboard/img/home.291c34d2.svg"
+  },
+  {
+    "revision": "97cf08755de6285a1bd3a79a76648172",
+    "url": "/ipl-dashboard/img/ipl-logo.97cf0875.png"
+  },
+  {
+    "revision": "e8cc4206efaaca47a535697107972f8a",
+    "url": "/ipl-dashboard/img/left-arrow.e8cc4206.svg"
+  },
+  {
+    "revision": "6719431d6075f3639ff5bd84a5a34b79",
+    "url": "/ipl-dashboard/img/location.6719431d.svg"
+  },
+  {
+    "revision": "73cc859f2dfa6f51c8b9d47d2445b333",
+    "url": "/ipl-dashboard/img/matches.73cc859f.svg"
+  },
+  {
+    "revision": "a351f85e4bd1cdefc5bc47f04d86bd01",
+    "url": "/ipl-dashboard/img/trivia.a351f85e.svg"
+  },
+  {
+    "revision": "9aa1a3bef324fd08b92ea7703d3491cc",
+    "url": "/ipl-dashboard/index.html"
+  },
+  {
+    "revision": "71a295b2484fedf8eeca",
+    "url": "/ipl-dashboard/js/app.4fa2cdcd.js"
+  },
+  {
+    "revision": "c0480434631ffce68964",
+    "url": "/ipl-dashboard/js/chunk-vendors.3790e592.js"
+  },
+  {
+    "revision": "02a86072abbe4fc7bdcc72dac52df778",
+    "url": "/ipl-dashboard/manifest.json"
+  },
+  {
+    "revision": "bbe6e84d79c08db131f3720b09eacbfa",
+    "url": "/ipl-dashboard/matches.csv"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ipl-dashboard/robots.txt"
+  }
+]);
