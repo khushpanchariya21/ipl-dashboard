fc9cd896a8220ad4c197a40d3af45703fe3f1927
Khush Panchariya <khushx@Khushs-MacBook-Pro.local>
2021-10-03 12:30:11 +0530

    updated dist

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/IPL-Dashboard-Atlan/precache-manifest.762ea8827f749732fb1ce780a907b7b0.js"
+  "/ipl-dashboard/precache-manifest.9efa0cf591f7e9663d8aca8fbc5c0c5a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "ipl-dashboard"});
